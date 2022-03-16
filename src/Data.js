da0d3784ebec0e23457f54f47633432fa7dc6fcc
KiimDoHyun <bzidol@naver.com>
2022-03-16 11:00:50 +0900

    init

--- src/Data.js
+++
@@ -1,8 +0,0 @@
-export const URL = {
-	DataGrid: "/datagrid",
-	PivotGrid: "/pivotgrid",
-	TreeList: "/treeList",
-	Scheduler: "/scheduler",
-	HtmlEditor: "/htmleditor",
-	HtmlEditor1: "/htmleditor",
-};
